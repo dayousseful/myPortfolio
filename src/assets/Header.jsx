b1e0a365a7baf22b8d84abbba488002d909addbe
dayousseful <77815660+dayousseful@users.noreply.github.com>
2023-09-02 01:00:54 -0400

    updated the about me box

--- src/assets/Header.jsx
+++ src/assets/Header.jsx
@@ -3,49 +3,47 @@ import ReactDOM  from 'react-dom/client'
 import '../style/Header.css'
 
 function Header(props) {
+  let Highlighted, filterHighlight, filterMenu, contButton
   const selectors = ['All', 'About', 'Projects', 'Media']
   const currentstate = props.currentstate
+  
   const renderButtons = selectors => {
     return selectors.map ((currentstate, index) => {
-      return ( <div key={index} className={'filter '+'filter'+currentstate} onClick={()=> props.setCurrentstate(currentstate)}>{currentstate}</div>) }) }
+      return ( <div key={index} className={'filter '+'filter'+currentstate} onClick={()=> props.setCurrentstate(currentstate)}>{currentstate}</div>) }) 
+  }
+  
+  if (currentstate === "Media") {
+    Highlighted = {width: 70, right: 4}
+  } else if(currentstate === "About") {
+    Highlighted = {width: 70, left :61}
+  } else if(currentstate === "Projects") {
+      Highlighted = {width: 84, right: 85}
+  }else{
+      Highlighted = {width: 55}
+  }
 
-        let Highlighted
-        if (currentstate === "Media") {
-          Highlighted = {width: 70, right: 4}
-        } else if(currentstate === "About") {
-          Highlighted = {width: 70, left :61}
-        } else if(currentstate === "Projects") {
-            Highlighted = {width: 84, right: 85}
-        }else{
-            Highlighted = {width: 55}
-        }
-       
-        let filterHighlight
-        let filterMenu
-        let contButton
-          if (props.colorState === true){
-              filterMenu = 'fmd'
-              filterHighlight = 'fhd'
-              contButton = 'cbd'
-          } else{
-              filterMenu = 'filter__menu'
-              filterHighlight = 'filter__highlight'
-              contButton = 'cb'
-          }
+  if (props.colorState === true){
+    filterMenu = 'filterMenuDark'
+    filterHighlight = 'filterHighlightDark'
+    contButton = 'cbd'
+  } else{
+    filterMenu = 'filterMenu'
+    filterHighlight = 'filterHighlight'
+    contButton = 'cb'
+  }
 
   return (
-        <div className='Header'>
-            <div className='header__logo'>
-            <img src={'src/images/logo.png'} className= 'image1' alt='logo'/>
-            </div>
-          <nav className={filterMenu}> 
-            <div className={filterHighlight} style={Highlighted} ></div>
-            {renderButtons(selectors)}
-          </nav> 
-          <a href='mailto:usfhamdy@yahoo.com' className={contButton}>
-            <span>Contact</span>
-          </a>
-        </div>
+    <div className='Header'>
+      <div className='header__logo'>
+      <img src={'src/images/logo.png'} className= 'image1' alt='logo'/>
+      </div>
+      <nav className={filterMenu}> 
+       <div className={filterHighlight} style={Highlighted} ></div>
+       {renderButtons(selectors)}
+      </nav> 
+      <a href='mailto:usfhamdy@yahoo.com' className={contButton}>
+       <span>Contact</span>
+      </a>
+    </div>
   )
-}
-  export default Header
+} export default Header
