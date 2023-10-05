55323fe12407c3ab8dfdfaca1ff8de62916e8870
dayousseful <77815660+dayousseful@users.noreply.github.com>
2023-10-05 19:00:07 -0400

    Connected Mongoose

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,5 +1,24 @@
 const express = require('express')
-const router = express.Router
+const router = express.Router()
+const schemas = require('../models/Schemas')
 
 
 console.log('running')
+
+router.post('/form', async(req,res) => {
+    const {name, email} = req.body
+    const userData = {name: name, email : email}
+    const newUser = new schemas.Users(userData)
+    const saveUser = await newUser.save()
+    if (saveUser){
+      res.send('form filled')
+    }
+    res.end()
+})
+
+router.get('/getReq',(req,res) => {
+    res.send('ya mosahel el 7al ya rab')
+})
+
+
+module.exports = router
---
+++ frontend/src/assets/AboutMe.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import ReactDOM  from 'react-dom/client'
+import '../style/Body.css'
+import { useState } from 'react'
+
+function AboutMe(props){
+  const [highlight, setHighlight] = useState(true)
+  const currentstate = props.currentstate
+  let dynoGrid, highlightStyle, aboutmeHighlight, aboutNavbar
+  if (currentstate === "Media" && window.innerWidth> 1000) {
+    dynoGrid = {gridRow: '2/4', gridColumn: '4/5', opacity:'0.2'}
+  } else if(currentstate === "About" && window.innerWidth> 1000) {
+    dynoGrid = {gridRow: '1/3', gridColumn: '4/5'}
+  } else if(currentstate === "Projects" && window.innerWidth> 1000) {
+    dynoGrid = {gridArea:'2 / 3 / 4 / 4' , opacity:'0.2'}
+  }else if(currentstate === "All" && window.innerWidth> 1000){
+    dynoGrid = {gridRow: '1/3', gridColumn: '4/5'}
+  }else if(currentstate === "All" && window.innerWidth< 1000){
+    dynoGrid = {gridRow: '3/5', gridColumn: '3/5'}
+  }else if(currentstate === "About" && window.innerWidth< 1000){
+    dynoGrid = {gridRow: '2/4', gridColumn: '3/5'}
+  }else if(currentstate === "Projects" && window.innerWidth< 1000){
+    dynoGrid = {gridRow: '6/8', gridColumn: '3/5', opacity:'0.1'}
+  }else if(currentstate === "Media" && window.innerWidth< 1000){
+    dynoGrid = {gridRow: '3/5', gridColumn: '3/5', opacity:'0.1'}
+  }
+  
+  
+  highlight ? highlightStyle = {left:4,width:70} : highlightStyle = {right:4,width:99}
+  
+  
+  function abouthighlightToggle(){highlight ? null : setHighlight(true)}
+  function inspohighlightToggle(){highlight ? setHighlight(false) : null }
+
+  if (props.colorState === false){
+    aboutNavbar = 'aboutmeNavBar'
+    aboutmeHighlight = 'aboutmeHighlight'
+  } else{
+    aboutNavbar = 'aboutmeNavBarDark'
+    aboutmeHighlight = 'aboutmeHighlightDark'
+  }
+  
+  return(
+    <div className={'card aboutMe'+props.carddark} style={dynoGrid}>
+      <nav className={aboutNavbar}>
+        <div className={aboutmeHighlight} style={highlightStyle}></div>
+        <div className='filter' style={{zIndex:1}} onClick={abouthighlightToggle}>About</div>
+        <div className='filter' style={{zIndex:1}} onClick={inspohighlightToggle}>Inspiration</div>
+      </nav>
+      <p>I am a recent graduate from the University of central Florida, with bachelors in Web design and social platforms
+      </p>
+      <p>I am a recent graduate from the University of central Florida, with bachelors in Web design and social platformI am a recent graduate from the University of central Florida, with bachelors in Web design and social platform
+      </p>
+    </div>
+  ) 
+
+}
+export default AboutMe
