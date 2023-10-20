9eaa8854b66914e1f05888baffc716a1fbbf955e
dayousseful <77815660+dayousseful@users.noreply.github.com>
2023-10-20 11:40:42 -0400

    cors update 20/1

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const router = require('./routes/router')
 const app = express()
+const schemas = require('./models/Schemas')
 const mongoose = require('mongoose')
 require('dotenv/config')
 
@@ -11,31 +12,22 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
 
 const corsOptions = {
-  origin: 'https://youssefhamdy.com',
+  origin: 'https://youssefhamdy.com/',
   credentials: true,
   optionSuccessStatus: 200
 }
 
+app.post('/form', cors(corsOptions), async(req,res) => {
+  const {name, email} = req.body
+  const userData = {name: name, email : email}
+  const newUser = new schemas.Users(userData)
+  const saveUser = await newUser.save()
+  if (saveUser){
+    res.send(true)
+  }
+  res.end()
+})
 
-// app.use((req, res, next) => {
-//     res.setHeader(
-//       "Access-Control-Allow-Origin",
-//       "https://youssefhamdy.com"
-//     );
-//     res.setHeader(
-//       "Access-Control-Allow-Methods",
-//       "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-//     );
-//     res.setHeader(
-//       "Access-Control-Allow-Headers",
-//       "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-//     );
-//     res.setHeader("Access-Control-Allow-Credentials", true);
-//     res.setHeader("Access-Control-Allow-Private-Network", true);
-//     //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-//     res.setHeader("Access-Control-Max-Age", 7200);
-//     next();
-// });
 
 app.use(cors(corsOptions))
 app.use('/', router)
@@ -52,6 +44,3 @@ const port = process.env.PORT
 const server = app.listen(port, () => {
     console.log(`server is running on port ${port}`)
 })
-
-
-
--- backend/routes/router.js
+++ backend/routes/router.js
@@ -5,16 +5,16 @@ const schemas = require('../models/Schemas')
 
 console.log('running')
 
-router.post('/form', async(req,res) => {
-  const {name, email} = req.body
-  const userData = {name: name, email : email}
-  const newUser = new schemas.Users(userData)
-  const saveUser = await newUser.save()
-  if (saveUser){
-    res.send(true)
-  }
-  res.end()
-})
+// router.post('/form', async(req,res) => {
+//   const {name, email} = req.body
+//   const userData = {name: name, email : email}
+//   const newUser = new schemas.Users(userData)
+//   const saveUser = await newUser.save()
+//   if (saveUser){
+//     res.send(true)
+//   }
+//   res.end()
+// })
 
 router.get('/getReq',(req,res) => {
   res.send('ya mosahel el 7al ya rab')
